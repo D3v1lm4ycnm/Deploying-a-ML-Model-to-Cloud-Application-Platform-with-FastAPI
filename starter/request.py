c212433c975de85667d88d14801220ba7a449aab
Your Name <you@example.com>
2024-05-28 22:39:54 +0700

    add screenshots and unit tests

--- starter/request.py
+++ starter/request.py
@@ -3,11 +3,12 @@ import requests
 import json
 
 
-BASE_URL = 'http://localhost:8000'  # Replace with your API URL
+BASE_URL = 'https://salary-predict-fastapi.azurewebsites.net'  # Replace with your API URL
 
 
 def test_get():
     response = requests.get(f'{BASE_URL}/')  # Replace with your GET endpoint
+    print(response.json())
     assert response.status_code == 200
     # Replace with your expected response
     assert response.json() == {'Hello': 'World'}
@@ -31,5 +32,10 @@ def test_post_prediction1():
         "native-country": "United-States"
     }
     response = requests.post(f'{BASE_URL}/predict', data=json.dumps(data))
+    print(response.json())
     assert response.status_code == 200
     assert response.json() == {'prediction': '<=50K'}
+
+if __name__ == '__main__':
+    test_get()
+    test_post_prediction1()
--- starter/test_main.py
+++ starter/test_main.py
@@ -36,21 +36,97 @@ def test_post_predict1():
 
 def test_post_predict2():
     data = {
-        "age": 39,
-        "workclass": "Private",
-        "fnlgt": 77516,
-        "education": "11th",
-        "education-num": 7,
-        "marital-status": "Never-married",
-        "occupation": "Machine-op-inspct",
-        "relationship": "Own-child",
-        "race": "Black",
+        "age": 50,
+        "workclass": "Self-emp-not-inc",
+        "fnlgt": 83311,
+        "education": "Bachelors",
+        "education-num": 13,
+        "marital-status": "Married-civ-spouse",
+        "occupation": "Exec-managerial",
+        "relationship": "Husband",
+        "race": "White",
         "sex": "Male",
         "capital-gain": 0,
         "capital-loss": 0,
-        "hours-per-week": 40,
+        "hours-per-week": 13,
+        "native-country": "United-States",
+    }
+    response = client.post("/predict", json=data)
+    assert response.status_code == 200
+    assert response.json() == {"prediction": "<=50K"}
+
+def test_post_predict3():
+    data = {
+        "age": 52,
+        "workclass": "Self-emp-not-inc",
+        "fnlgt": 209642,
+        "education": "HS-grad",
+        "education-num": 9,
+        "marital-status": "Married-civ-spouse",
+        "occupation": "Exec-managerial",
+        "relationship": "Husband",
+        "race": "White",
+        "sex": "Male",
+        "capital-gain": 0,
+        "capital-loss": 0,
+        "hours-per-week": 45,
+        "native-country": "United-States"
+    }
+    response = client.post("/predict", json=data)
+    assert response.status_code == 200
+    assert response.json() == {"prediction": ">50K"}
+
+def test_post_predict4():
+    data = {
+        "age": 31,
+        "workclass": "Private",
+        "fnlgt": 45781,
+        "education": "Masters",
+        "education-num": 14,
+        "marital-status": "Never-married",
+        "occupation": "Prof-specialty",
+        "relationship": "Not-in-family",
+        "race": "White",
+        "sex": "Female",
+        "capital-gain": 14084,
+        "capital-loss": 0,
+        "hours-per-week": 50,
         "native-country": "United-States"
     }
     response = client.post("/predict", json=data)
     assert response.status_code == 200
     assert response.json() == {"prediction": "<=50K"}
+
+def test_post_predict5():
+    data = {
+        "age": 31,
+        "workclass": "Private",
+        "fnlgt": 45781,
+        "education": "Masters",
+        "education-num": 14,
+        "marital-status": "Never-married",
+        "occupation": "Prof-specialty",
+        "relationship": "Not-in-family",
+    }
+    response = client.post("/predict", json=data)
+    assert response.status_code == 400
+
+def test_post_predict6():
+    data = {
+        "age": 31,
+        "workclass": "Private",
+        "fnlgt": 45781,
+        "education": "Masters",
+        "education-num": 14,
+        "marital-status": "Never-married",
+        "occupation": "Prof-specialty",
+        "relationship": "Not-in-family",
+        "race": "White",
+        "sex": "Female",
+        "capital-gain": 14084,
+        "capital-loss": 0,
+        "hours-per-week": 50,
+        "native-country": "United-States"
+    }
+    response = client.post("/predit", json=data)
+    assert response.status_code == 404
